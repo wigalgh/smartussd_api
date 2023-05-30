9659ef98dcfafae41666e36eac101eccc155adec
eani <eani286@gmail.com>
2023-05-30 14:36:36 +0000

    update on callback function

--- callbackurl.php
+++ callbackurl.php
@@ -3,11 +3,10 @@
 	$time=date("Y/m/d h:i:s");
 	$data =  @file_get_contents('php://input'); //get callback response from Redde
 
-
 	//log data into appropriate text file
 	@file_put_contents('logs.txt',"Callback response log on ". $time ."=> ". $data . "\n",FILE_APPEND);
 	@file_put_contents('Transactions.txt',"Callback response on ".$time ."=> ". $data. "\n" ,FILE_APPEND);
-
+	alllogs_log("Callback response log on " , $time , $data);  //Create the log folder and write the logs according to their date
 
 	//decode the data and proccess it
 	$recieveddata = json_decode($data);
